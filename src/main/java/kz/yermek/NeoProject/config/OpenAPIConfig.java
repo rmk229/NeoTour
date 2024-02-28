e8af188514c166720ab98a22eedc14caf9b2be39
Ermek Shmanov <erma_99-22@mail.ru>
2024-02-28 11:49:46 +0600

    last

--- src/main/java/kz/yermek/NeoProject/config/OpenAPIConfig.java
+++ src/main/java/kz/yermek/NeoProject/config/OpenAPIConfig.java
@@ -5,39 +5,35 @@ import io.swagger.v3.oas.annotations.info.Contact;
 
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@OpenAPIDefinition(
-//        info = @Info(
-//                contact = @Contact(
-//                        name = "yermek",
-//                        email = "shmanovermek@gmail.com"
-//                ),
-//                description = "OpenAPI documentation",
-//                title = "OpenAPI specification - yermek",
-//                version = "1.0"
-//        ),
-//        servers = {
-//                @Server(
-//                        description = "Local ENV",
-//                        url = "http://localhost:8000"
-//                ),
-//                @Server(
-//                        description = "Team city Cloud",
-//                        url = "http://172.20.10.2:8000" // TODO?????
-//                )
-//        }
-//)
-@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "yermek",
+                        email = "shmanovermek@gmail.com"
+                ),
+                description = "OpenAPI documentation",
+                title = "OpenAPI specification - yermek",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Api Title",
+                        url = "https://neotour-production-8df4.up.railway.app/" // TODO?????
+                )
+        }
+)
 public class OpenAPIConfig {
-    @Bean
-    public OpenAPI usersMicroserviceOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Neo tour API Title")
-                        .description("Description")
-                        .version("1.0"));
-    }
+//    @Bean
+//    public OpenAPI usersMicroserviceOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info().title("Neo tour API Title")
+//                        .description("Description")
+//                        .version("1.0"));
+//    }
 }
 
